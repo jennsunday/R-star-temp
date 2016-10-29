e12f9bd9c2ccd28af2f012583379189aa55c0964
Jennifer Sunday <Sunday@biodiversity.ubc.ca>
2016-10-28 17:52:58 -0700

    more updates

--- scripts/03_plots_of_R-star_K_notlogscale.R
+++ scripts/03_plots_of_R-star_K_notlogscale.R
@@ -32,7 +32,7 @@ Rstar_expt30<-subset(Rstar_expt, Rstar_expt$temperature==30)
 Rstar_expt10$start_time
 #### Step 3: plot growth and nitrate for both species by temperature####
 pdf(file="./figures/Growth_nitrate_biovolume_across_time_and_temp_notlog.pdf", width = 8, height = 8)
-par(mfrow=c(2,4))
+par(mfrow=c(3,4))
 par(mar=c(3,3,0.5, 0.5), oma=c(0,0,0,0))
 with(Rstar_expt10, plot((cell_count)~start_time, type="n", main="10", ylab="", xlab="", ylim=c(0, 300000), xlim=c(min(start_time), max(start_time))))
 with(subset(Rstar_expt10, Rstar_expt10$species=="CH"), points((cell_count)~start_time, col=1))
@@ -54,6 +54,54 @@ with(subset(Rstar_expt30, Rstar_expt30$species=="TT"), points((cell_count)~start
 
 (Rstar_expt25$start_time)
 #
+
+
+
+with(Rstar_expt10, plot((volume)~start_time, type="n", main="10", ylab="", xlab="", ylim=c(0, 1500)))
+with(subset(Rstar_expt10, Rstar_expt10$species=="CH"), points((volume)~start_time, col=1))
+with(subset(Rstar_expt10, Rstar_expt10$species=="TT"), points((volume)~start_time, col=2))
+with(subset(Rstar_expt10, Rstar_expt10$species=="BB"), points((volume)~start_time, col=3))
+mtext(side = 2, line = 2, 'cell size')
+legend("topright", pch=1, col=c(1,2), c("CH", "TT"))
+
+with(Rstar_expt16, plot((volume)~start_time, ylab="", xlab="", type="n", main="16", ylim=c(0, 1500)))
+with(subset(Rstar_expt16, Rstar_expt16$species=="CH"), points((volume)~start_time, col=1))
+with(subset(Rstar_expt16, Rstar_expt16$species=="TT"), points((volume)~start_time, col=2))
+with(subset(Rstar_expt16, Rstar_expt16$species=="BB"), points((volume)~start_time, col=3))
+
+
+with(Rstar_expt25, plot((volume)~start_time, ylab="", xlab="", type="n", main="25", ylim=c(0, 1500)))
+with(subset(Rstar_expt25, Rstar_expt25$species=="CH"), points((volume)~start_time, col=1))
+with(subset(Rstar_expt25, Rstar_expt25$species=="TT"), points((volume)~start_time, col=2))
+with(subset(Rstar_expt25, Rstar_expt25$species=="BB"), points((volume)~start_time, col=3))
+
+with(Rstar_expt30, plot((volume)~start_time, ylab="", xlab="", type="n", main="30", ylim=c(0, 1500)))
+with(subset(Rstar_expt30, Rstar_expt30$species=="CH"), points((volume)~start_time, col=1))
+with(subset(Rstar_expt30, Rstar_expt30$species=="TT"), points((volume)~start_time, col=2))
+with(subset(Rstar_expt30, Rstar_expt30$species=="BB"), points((volume)~start_time, col=3))
+
+
+#
+# biovolume data
+
+with(Rstar_expt10, plot((biovolume)~start_time, type="n", ylab="", xlab="", ylim=c(0, 1.5E8)))
+with(subset(Rstar_expt10, Rstar_expt10$species=="CH"), points((biovolume)~start_time, col=1))
+with(subset(Rstar_expt10, Rstar_expt10$species=="TT"), points((biovolume)~start_time, col=2))
+mtext(side = 2, line = 2, 'biovolume')
+legend("topleft", pch=1, col=c(1,2), c("CH", "TT"))
+
+with(Rstar_expt16, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
+with(subset(Rstar_expt16, Rstar_expt16$species=="CH"), points((biovolume)~start_time, col=1))
+with(subset(Rstar_expt16, Rstar_expt16$species=="TT"), points((biovolume)~start_time, col=2))
+
+with(Rstar_expt25, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
+with(subset(Rstar_expt25, Rstar_expt25$species=="CH"), points((biovolume)~start_time, col=1))
+with(subset(Rstar_expt25, Rstar_expt25$species=="TT"), points((biovolume)~start_time, col=2))
+
+with(Rstar_expt30, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
+with(subset(Rstar_expt30, Rstar_expt30$species=="CH"), points((biovolume)~start_time, col=1))
+with(subset(Rstar_expt30, Rstar_expt30$species=="TT"), points((biovolume)~start_time, col=2))
+
 #add nitrate data
 #
 #par(mfrow=c(1,4))
@@ -81,26 +129,6 @@ with(subset(nitratedata30, nitratedata30$species=="CH"), points(abs~date_N_assay
 with(subset(nitratedata30, nitratedata30$species=="TT"), points(abs~date_N_assay, col=2, pch=2))
 mtext(side = 3, line = 1.5, '30°C')
 
-#
-# biovolume data
-
-with(Rstar_expt10, plot((biovolume)~start_time, type="n", ylab="", xlab="", ylim=c(0, 1.5E8)))
-with(subset(Rstar_expt10, Rstar_expt10$species=="CH"), points((biovolume)~start_time, col=1))
-with(subset(Rstar_expt10, Rstar_expt10$species=="TT"), points((biovolume)~start_time, col=2))
-mtext(side = 2, line = 2, 'biovolume')
-legend("topleft", pch=1, col=c(1,2), c("CH", "TT"))
-
-with(Rstar_expt16, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
-with(subset(Rstar_expt16, Rstar_expt16$species=="CH"), points((biovolume)~start_time, col=1))
-with(subset(Rstar_expt16, Rstar_expt16$species=="TT"), points((biovolume)~start_time, col=2))
-
-with(Rstar_expt25, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
-with(subset(Rstar_expt25, Rstar_expt25$species=="CH"), points((biovolume)~start_time, col=1))
-with(subset(Rstar_expt25, Rstar_expt25$species=="TT"), points((biovolume)~start_time, col=2))
-
-with(Rstar_expt30, plot((biovolume)~start_time, ylab="", xlab="", type="n", ylim=c(0, 1.5E8)))
-with(subset(Rstar_expt30, Rstar_expt30$species=="CH"), points((biovolume)~start_time, col=1))
-with(subset(Rstar_expt30, Rstar_expt30$species=="TT"), points((biovolume)~start_time, col=2))
 
 dev.off()
 
